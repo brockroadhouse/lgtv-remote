43b4118bc516d9e390595a63a523c5381f9f22f2
Brock Roadhouse <brockroadhouse@gmail.com>
2022-06-17 22:23:59 -0700

    only hdmi inputs and buttons work

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # lgtv-remote
 
 ## To run:
+
+Set IP of tv in .env file:
+```
+TV_IP=192.168.1.100
+```
+
+Set any flask environment variables:
+```
+FLASK_APP=lgtv-remote
+FLASK_ENV=development
+FLASK_RUN_PORT=5005
+```
+
+Run the following commands:
+
 ```
 python3 -m venv [virtual_environment_dir]
 source [virtual_environment_dir]/bin/activate
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import json
 from flask import Flask, session,render_template
+from os import environ
 from pywebostv.connection import WebOSClient
-from pywebostv.controls import InputControl, SourceControl
+from pywebostv.controls import InputControl, SourceControl, MediaControl
 
 config = {
     "DEBUG": True,          # some Flask specific configs
@@ -10,24 +11,16 @@ config = {
 }
 app = Flask(__name__)
 app.secret_key = '90234jkfn08s8ejS'
+TV_IP=environ.get('TV_IP')
 
 @app.before_first_request
 def before_first_request_func():
-    store = {}
-    client = WebOSClient("192.168.1.89")
-    client.connect()
-    for status in client.register(store):
-        if status == WebOSClient.PROMPTED:
-            print("Please accept the connect on the TV!")
-        elif status == WebOSClient.REGISTERED:
-            print("Registration successful!")
-
-    session['store'] = store
+    get_client()
 
 @app.context_processor
 def inject_input_sources():
-    sources = get_sources()
-    return dict(sources=enumerate(map(lambda s: s.label, sources)))
+    hdmi = get_hdmi_labels()
+    return dict(sources=enumerate(hdmi))
 
 @app.route('/')
 def index():
@@ -36,9 +29,28 @@ def index():
 @app.route('/hdmi/<id>')
 def hdmi(id):
     sc = get_source_control()
-    sc.set_source(sc.list_sources()[id])
+    hdmis = list(get_hdmi_inputs())
+    sc.set_source(hdmis[int(id)])
     return json.dumps('success')
 
+@app.route('/volUp')
+def volUp():
+    mc = get_media_control()
+    mc.volume_up()
+    return 'Volume up.'
+
+@app.route('/volDown')
+def volDown():
+    mc = get_media_control()
+    mc.volume_down()
+    return 'Volume down.'
+
+@app.route('/mute')
+def mute():
+    mc = get_media_control()
+    muted = mc.get_volume()['volumeStatus']['muteStatus']
+    mc.mute(not muted)
+    return mc.get_volume()
 
 @app.route('/home')
 def home():
@@ -60,6 +72,12 @@ def dash():
     run_input('dash')
     return "Pressed."
 
+@app.route('/button/<command>')
+def button(command):
+    # DANGEROURS
+    run_input(command)
+    return "Pressed."
+
 def run_input(command):
     client = get_client()
 
@@ -68,25 +86,36 @@ def run_input(command):
     getattr(inp, command)()
     inp.disconnect_input()
 
+def get_hdmi_labels():
+    sources = get_hdmi_inputs()
+    return list(source['label'] for source in sources)
+
+def get_hdmi_inputs():
+    sources = get_sources()
+    return (source for source in sources if(source['id'].startswith('HDMI')))    
+
 def get_sources(client=False):
     client = client or get_client()
     return get_source_control(client).list_sources()
+
+def get_media_control():
+    return MediaControl(get_client())
 
 def get_source_control(client=False):
     client = client or get_client()
     return SourceControl(client)
 
 def get_client():
-    print('gettingclient')
     store = session.get('store', {})
-    client = WebOSClient("192.168.1.89")
+    client = WebOSClient(TV_IP)
     client.connect()
     for status in client.register(store):
-        if status == WebOSClient.PROMPTED:
-            print("Please accept the connect on the TV!")
-        elif status == WebOSClient.REGISTERED:
-            print("Registration successful!")
-
+        print(status)
+        # if status == WebOSClient.PROMPTED:
+        #     #log prompted
+        # elif status == WebOSClient.REGISTERED:
+        #     #log registrers
+    session['store'] = store
     return client
 
 if __name__ == "__main__":
